bf790316887bff22a04b620ee41038eaddb884dd
anichu <anis.cse5.bu@gmail.com>
2023-06-17 12:03:53 +0600

    initialize winners route

--- src/pages/Winners/Winners.js
+++ src/pages/Winners/Winners.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Winners = () => {
-	return <div></div>;
+	return (
+		<div className="pt-[90px] pb-5">
+			<h1 className="text-white text-xl font-bold text-center">
+				Coming soon...
+			</h1>
+		</div>
+	);
 };
 
 export default Winners;
